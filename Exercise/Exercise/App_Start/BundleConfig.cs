f2a025e30c4fac173b0e9dc88e1c09d10bdb9746
jocamina <neosdiez@gmail.com>
2017-01-21 13:01:02 +0000

    adding angular from NuGet

--- Exercise/Exercise/App_Start/BundleConfig.cs
+++ Exercise/Exercise/App_Start/BundleConfig.cs
@@ -9,14 +9,24 @@ namespace Exercise
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js",
+                "~/Scripts/angular-resource.js",
+                "~/Scripts/angular-route.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularapp").Include(
+                "~/Scripts/app/app.js",
+                "~/Scripts/app/MainController.js",
+                "~/Scripts/app/MainServices.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
